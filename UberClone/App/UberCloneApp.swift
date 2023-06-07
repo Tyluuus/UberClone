7eac182e39b68923a41d29330b082c676632083e
Piotr Tyl <tyl.piotrr@gmail.com>
2023-06-07 12:06:41 +0200

    Initial Commit

---
+++ UberClone/App/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Piotr Tyl on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
