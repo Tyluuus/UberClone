8ec171b9e1c368aa53756fc7d91dbb01bc8511af
Piotr Tyl <tyl.piotrr@gmail.com>
2023-06-09 16:28:29 +0200

    Project upload

---
+++ UberClone/Models/MapViewState.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewState.swift
+//  UberClone
+//
+//  Created by Piotr Tyl on 09/06/2023.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locationSelected
+    case polylineAdded
+}
---
+++ UberClone/Models/UberLocation.swift
@@ -0,0 +1,13 @@
+//
+//  UberLocation.swift
+//  UberClone
+//
+//  Created by Piotr Tyl on 09/06/2023.
+//
+
+import CoreLocation
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
